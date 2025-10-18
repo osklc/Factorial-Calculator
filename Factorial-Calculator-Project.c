3202d7c946f872bb49d3b68a1dbee2a67a017689
osklc <osklcmail@gmail.com>
2025-10-18 15:01:47 +0300

    Sınırlayıcı değer magic number yapıldı, fail-fast prensibine uygun olarak düzeltildi. UX(Kullanıcı Deneyimi) için iyileştirme yapıldı

--- Factorial-Calculator-Project.c
+++ Factorial-Calculator-Project.c
@@ -10,40 +10,45 @@ int main()
 
 	int sayi;
 	unsigned long long int faktoriyel_sonucu=1;
+	const int MAX_FAKTORIYEL_SINIRI = 20;
 	setlocale(LC_ALL,"Turkish");
 	
-	printf("Lütfen Faktöriyel Hesabý Ýçin Pozitif Bir Tam Sayý Giriniz (Ondalýk Sayýnýn Tam Kýsmý Kullanýlacaktýr): ");
+	printf("Lütfen Faktöriyel Hesabý Ýçin 0-20 Arasýnda Bir Tam Sayý Giriniz (Ondalýk Sayýnýn Tam Kýsmý Kullanýlacaktýr): ");
 	scanf("%d",&sayi);
-	printf("--------------------------------\n");
 	
-	int i = sayi;
+	if(sayi==0 || sayi==1)
+	{
+		printf("--------------------------------");
+		printf("\nSonuç: %llu",faktoriyel_sonucu);
+		return 0;
+	}
 	
 	if (sayi<0)
 	{
-		printf("Lütfen Pozitif Bir Tam Sayý Giriniz!");
-		return 1;
-	}
-	
-	else if (sayi>20)
-	{
-		printf("Bu Program 20'den Büyük Sayýlarýn Faktöriyelini Alamaz!");
-		return 1;
-	}
-	
-	else
-	{
-		while (1<i)
-		{
-			faktoriyel_sonucu=i*faktoriyel_sonucu;
-			i--;
-			printf("%llu\n",faktoriyel_sonucu);
-			Sleep(100);
-		}
-		
 		printf("--------------------------------");
-		printf("\nSonuç: %llu",faktoriyel_sonucu);
-	
+		printf("\nLütfen Pozitif Bir Tam Sayý Giriniz!");
+		return 1;
 	}
+	
+	if (sayi>MAX_FAKTORIYEL_SINIRI)
+	{
+		printf("--------------------------------");
+		printf("\nBu Program 20'den Büyük Sayýlarýn Faktöriyelini Alamaz!");
+		return 1;
+	}
+	
+	printf("--------------------------------\n");
+	int i = sayi;
+	while (1<i)
+	{
+		faktoriyel_sonucu=i*faktoriyel_sonucu;
+		i--;
+		printf("%llu\n",faktoriyel_sonucu);
+		Sleep(100);
+	}
+		
+	printf("--------------------------------");
+	printf("\nSonuç: %llu",faktoriyel_sonucu);
 	
 	return 0;
 
